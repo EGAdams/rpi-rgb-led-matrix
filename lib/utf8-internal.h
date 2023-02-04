c1c176542fa5258e5346309033d9b4e54666d865
adamsl <eg1972@gmail.com>
2023-02-04 16:48:46 -0500

    put utf8-internal.h back

--- lib/utf8-internal.h
+++ lib/utf8-internal.h
@@ -1,62 +1,57 @@
+// -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+// Copyright (C) 2013 Henner Zeller <h.zeller@acm.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 2.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://gnu.org/licenses/gpl-2.0.txt>
 #ifndef RPI_GRAPHICS_UTF8_H
 #define RPI_GRAPHICS_UTF8_H
 
 #include <stdint.h>
 
-// Utility function that reads and decodes UTF-8 encoded characters from a string.
-// Assumes that the string is encoded correctly in UTF-8 format.
+// Utility function that reads UTF-8 encoded codepoints from byte iterator.
+// No error checking, we assume string is UTF-8 clean.
 template <typename byte_iterator>
-uint32_t next_UTF8_codepoint( byte_iterator &string_iterator ) {
-uint32_t codepoint = string_iterator++;
-// Check if the first byte of the character is one byte long (7 bits).
-if ( codepoint < 0x80 ) {
-    return codepoint; // Iterator already incremented.
+uint32_t utf8_next_codepoint(byte_iterator &it) {
+  uint32_t cp = *it++;
+  if (cp < 0x80) {
+    return cp;   // iterator already incremented.
+  }
+  else if ((cp & 0xE0) == 0xC0) {
+    cp = ((cp & 0x1F) << 6) + (*it & 0x3F);
+  }
+  else if ((cp & 0xF0) == 0xE0) {
+    cp = ((cp & 0x0F) << 12) + ((*it & 0x3F) << 6);
+    cp += (*++it & 0x3F);
+  }
+  else if ((cp & 0xF8) == 0xF0) {
+    cp = ((cp & 0x07) << 18) + ((*it & 0x3F) << 12);
+    cp += (*++it & 0x3F) << 6;
+    cp += (*++it & 0x3F);
+  }
+  else if ((cp & 0xFC) == 0xF8) {
+    cp = ((cp & 0x03) << 24) + ((*it & 0x3F) << 18);
+    cp += (*++it & 0x3F) << 12;
+    cp += (*++it & 0x3F) << 6;
+    cp += (*++it & 0x3F);
+  }
+  else if ((cp & 0xFE) == 0xFC) {
+    cp = ((cp & 0x01) << 30) + ((*it & 0x3F) << 24);
+    cp += (*++it & 0x3F) << 18;
+    cp += (*++it & 0x3F) << 12;
+    cp += (*++it & 0x3F) << 6;
+    cp += (*++it & 0x3F);
+  }
+  ++it;
+  return cp;
 }
 
-// Check if the character is two bytes long (11 bits).
-/*
-    The code next fragment ( else if ) is checking for 11 bits in the bottom of the codepoint variable, 
-    which represents two bytes of UTF-8 data. If the value is equal to 0xC0 (11000000 in binary) 
-    then the next 6 bits in the codepoint variable are shifted 6 bits to the left and are combined 
-    with the 6 least significant bits of the string iterator variable. The resulting value is the 
-    decoded code point.
-*/
-else if (( codepoint & 0xE0 ) == 0xC0 ) {
-
-    // the next line of code shifts the next 6 bits in the codepoint variable to the left
-    // and then combines the result with the 6 least significant bits of the string iterator variable.
-    codepoint = (( codepoint & 0x1F ) << 6 ) + ( string_iterator & 0x3F );
-
-} else if (( codepoint & 0xF0 ) == 0xE0 ) { 
-    
-    // Check if the character is three bytes long (16 bits).
-    codepoint = (( codepoint & 0x0F) << 12) + (( string_iterator & 0x3F ) << 6 );
-    codepoint += ( ++string_iterator & 0x3F );
-}
-
-// Check if the character is four bytes long (21 bits).
-else if (( codepoint & 0xF8) == 0xF0) {
-    codepoint = (( codepoint & 0x07) << 18) + (( string_iterator & 0x3F ) << 12 );
-    codepoint += ( ++string_iterator & 0x3F ) << 6;
-    codepoint += ( ++string_iterator & 0x3F );
-}
-// Check if the character is five bytes long (26 bits).
-else if (( codepoint & 0xFC) == 0xF8) {
-    codepoint = (( codepoint & 0x03) << 24 ) + (( string_iterator & 0x3F ) << 18 );
-    codepoint += ( ++string_iterator & 0x3F ) << 12;
-    codepoint += ( ++string_iterator & 0x3F ) << 6;
-    codepoint += ( ++string_iterator & 0x3F );
-}
-// Check if the character is six bytes long (31 bits).
-else if (( codepoint & 0xFE) == 0xFC) {
-    codepoint = (( codepoint & 0x01) << 30) + (( string_iterator & 0x3F ) << 24 );
-    codepoint += ( ++string_iterator & 0x3F ) << 18;
-    codepoint += ( ++string_iterator & 0x3F ) << 12;
-    codepoint += ( ++string_iterator & 0x3F ) << 6;
-    codepoint += ( ++string_iterator & 0x3F );
-}
-++string_iterator;
-return codepoint;
-}
-
-#endif // RPI_GRAPHICS_UTF8_H
+#endif  // RPI_GRAPHICS_UTF8_H
