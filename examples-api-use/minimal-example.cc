731e0198d5663d660fc91a426f57eda2920157a9
adamsl <eg1972@gmail.com>
2023-02-06 14:21:19 -0500

    add y loop

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -52,12 +52,14 @@ static void DrawLine(Canvas *canvas)
     int y_position = 0;
 
     for ( int test_count = 0; test_count < 50; test_count++ ) {
-        for ( x_position = 0; x_position < 512; x_position++ ) {
-            canvas->Fill( 0, 0, 0 );
-            canvas->SetPixel( x_position, y_position, 0, 255, 0 );
-            // printf( "canvas width: %d  canvas height: %d \n", canvas->width(), canvas->height() );
-            usleep ( 20 * 1000 );
-        }
+        for ( y_position = 0; y_position < 32; y_position++ ) {
+            for ( x_position = 0; x_position < 64; x_position++ ) {
+                canvas->Fill( 0, 0, 0 );
+                canvas->SetPixel( x_position, y_position, 0, 255, 0 );
+                // printf( "canvas width: %d  canvas height: %d \n", canvas->width(), canvas->height() );
+                usleep ( 5 * 1000 );
+            }
+        }    
     } // wait a little to slow down things.
 
     printf("done drawing lines");
