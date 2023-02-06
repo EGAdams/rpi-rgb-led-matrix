40e740a5041bc693cfddc098346984404cb588ed
adamsl <eg1972@gmail.com>
2023-02-06 11:31:12 -0500

    start at matrix_x = x and matrix_y = y

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -228,22 +228,9 @@ namespace rgb_matrix
                                             int x, int y,
                                             int *matrix_x, int *matrix_y) const
             {
-                const int panel_height = matrix_height / parallel_;
-                const int visible_width = (matrix_width / 64) * 32;
-                const int slab_height = 2 * panel_height; // one folded u-shape
-                const int base_y = (y / slab_height) * panel_height;
-                y %= slab_height;
-                if (y < panel_height)
-                {
-                    x += matrix_width / 2;
-                }
-                else
-                {
-                    x = visible_width - x - 1;
-                    y = slab_height - y - 1;
-                }
+                
                 *matrix_x = x;
-                *matrix_y = base_y + y;
+                *matrix_y = y;
             }
 
         private:
