26cbd909fb5139aa0f1817d8cbf16d30d3de04da
adamsl <eg1972@gmail.com>
2023-02-06 10:12:57 -0500

    print values

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -226,13 +226,23 @@ namespace rgb_matrix
 
             virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
                                             int x, int y,
-                                            int *matrix_x, int *matrix_y) const
-            {
+                                            int *matrix_x, int *matrix_y) const {
+
                 const int panel_height = matrix_height / parallel_;
-                const int visible_width = ( matrix_width / 64 ) * 32;
+                
+                printf( "panel_height=%d parallel=%d      ", panel_height, parallel_     );
+                printf( "matrix_width=%d matrix_height=%d ", matrix_width, matrix_height );
+                printf( "x=%d y=%d", x, y );
+
+                const int visible_width = ( matrix_width / 64 ) * 32; printf( "visible_width=%d", visible_width );
+                
                 const int slab_height = 2 * panel_height; // one folded u-shape
-                const int base_y = (y / slab_height) * panel_height;
-                y %= slab_height;
+                printf( "slab_height=%d", slab_height );
+
+                const int base_y = (y / slab_height) * panel_height; printf( "base_y=%d", base_y );
+                
+                y %= slab_height; printf( "y=%d", y );
+                
                 if (y < panel_height) {
                     x += matrix_width / 2;
                     y -= 8; // 8px offset between panels
@@ -240,8 +250,11 @@ namespace rgb_matrix
                     x = visible_width - x - 1;
                     y = slab_height - y - 1 - 8; // 8px offset between panels
                 }
+
                 *matrix_x = x;
                 *matrix_y = base_y + y;
+
+                printf( "Final Output: matrix_x=%d matrix_y=%d", *matrix_x, *matrix_y );
             }
 
         private:
