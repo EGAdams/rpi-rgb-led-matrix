596feed6e77045b4a6e0a9701bd0c36380600dd9
adamsl <eg1972@gmail.com>
2023-02-06 12:23:17 -0500

    speed up

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -57,7 +57,7 @@ static void DrawLine(Canvas *canvas)
         {
             canvas->Fill(0, 0, 0);
             canvas->SetPixel(x_position, y_position, 0, 255, 0);
-            usleep(500 * 1000);
+            usleep(50 * 1000);
         }
     } // wait a little to slow down things.
 
