dbe151b013f4e0fd9228eaa56b102210329f3228
adamsl <eg1972@gmail.com>
2023-02-06 13:21:28 -0500

    tak copilot for x >= 16

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -231,7 +231,10 @@ namespace rgb_matrix
                 if( x < 16 ) {
                     *matrix_x = x + 16;
                     *matrix_y = y;
-                    printf( "matrix_x=%d matrix_y=%d\n", *matrix_x, *matrix_y );
+                } else if( x >= 16 ) {
+                    *matrix_x = x - 16;
+                    *matrix_y = y;
+
                 } else {
                     *matrix_x = x;
                     *matrix_y = y;
