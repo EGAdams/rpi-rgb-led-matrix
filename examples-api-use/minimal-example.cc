609439366476b94c781a73fed5090cf24270217d
adamsl <eg1972@gmail.com>
2023-02-07 10:22:20 -0500

    increase test loop to 500

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -51,7 +51,7 @@ static void DrawLine(Canvas *canvas)
     int x_position = 0;
     int y_position = 0;
 
-    for ( int test_count = 0; test_count < 50; test_count++ ) {
+    for ( int test_count = 0; test_count < 500; test_count++ ) {
         for ( y_position = 0; y_position < 1; y_position++ ) {
             for ( x_position = 0; x_position < 64; x_position++ ) {
                 canvas->Fill( 0, 0, 0 );
