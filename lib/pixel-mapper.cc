23e52ae901957c15945cb1283d04a4d0801607b2
adamsl <eg1972@gmail.com>
2023-02-08 13:37:34 -0500

    added all y + 1s

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -257,14 +257,14 @@ namespace rgb_matrix
                     *matrix_x = x + 80 - 32;
                     y_not_set_yet = setMatrixY( y + 1, matrix_y );
                 } else if( x < 96 ) {
-                    *matrix_x = x + 96 -32;
-                    y_not_set_yet = setMatrixY( y+ 1, matrix_y );
+                    *matrix_x = x + 96 - 32;
+                    y_not_set_yet = setMatrixY( y + 1, matrix_y );
                 } else if( x < 112 ) {
                     *matrix_x = x + 112 - 32;
-                    y_not_set_yet = setMatrixY( y, matrix_y );
+                    y_not_set_yet = setMatrixY( y + 1, matrix_y );
                 } else if( x < 128 ) {
                     *matrix_x = x + 128 - 32;
-                    y_not_set_yet = setMatrixY( y, matrix_y );
+                    y_not_set_yet = setMatrixY( y + 1, matrix_y );
                 } else if( x < 144 ) {
                     *matrix_x = x + 144;
                     y_not_set_yet = setMatrixY( y, matrix_y );
