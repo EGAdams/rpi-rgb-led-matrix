c218540d1fedc052eb39e37a0fee7888f65aceb8
adamsl <eg1972@gmail.com>
2023-02-09 13:19:02 -0500

    256 64

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -232,8 +232,8 @@ namespace rgb_matrix
                         *matrix_x = x + 16;
                         *matrix_y = y;
                     } else if ( y < 16 ) {
-                        *matrix_x = x + 128;
-                        *matrix_y = y + 48;
+                        *matrix_x = x + 256;
+                        *matrix_y = y + 64;
                     } else {
                         *matrix_x = x;
                         *matrix_y = y;
