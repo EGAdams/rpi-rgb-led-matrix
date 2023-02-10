d21c9884997d57a4731bf2b40465df7030292a5c
adamsl <eg1972@gmail.com>
2023-02-10 10:41:51 -0500

    slow down

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -55,7 +55,7 @@ static void DrawLine(Canvas *canvas)
                 // canvas->Fill( 0, 0, 0 ); 
                 canvas->SetPixel( x_position, y_position, 0, 255, 0 );
                 // printf( "canvas width: %d  canvas height: %d \n", canvas->width(), canvas->height() );
-                usleep ( 1 * 1000 );
+                usleep ( 5 * 1000 );
             }
         }
         usleep ( 1 * 1000 * 1000 );
