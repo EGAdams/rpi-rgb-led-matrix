84c9bec5d3f5d67d3416b458618234a68cb2447a
adamsl <eg1972@gmail.com>
2023-02-10 09:21:06 -0500

    chage time to 5

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -58,7 +58,7 @@ static void DrawLine(Canvas *canvas)
                 usleep ( 1 * 1000 );
             }
         }
-        usleep ( 1 * 1000 * 1000 );
+        usleep ( 5 * 1000 * 1000 );
         canvas->Fill( 0, 0, 0 );    
     } // wait a little to slow down things.
 
