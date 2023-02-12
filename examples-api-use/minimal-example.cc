cf2448d2dbf28b6f04e13c6fa50f9aaa24705ad0
adamsl <eg1972@gmail.com>
2023-02-12 18:49:16 -0500

    512

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -50,8 +50,8 @@ static void DrawLine(Canvas *canvas)
     canvas->Fill(0, 0, 0);
     
     for ( int test_count = 0; test_count < 500; test_count++ ) {
-        for ( int y_position = 0; y_position < 1024; y_position++ ) {
-            for ( int x_position = 0; x_position < 1024; x_position++ ) {
+        for ( int y_position = 0; y_position < 512; y_position++ ) {
+            for ( int x_position = 0; x_position < 512; x_position++ ) {
                 // canvas->Fill( 0, 0, 0 ); 
                 canvas->SetPixel( x_position, y_position, 0, 255, 0 );
                 // printf( "canvas width: %d  canvas height: %d \n", canvas->width(), canvas->height() );
