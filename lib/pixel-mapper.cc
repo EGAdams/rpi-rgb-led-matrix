5a41539bee06afef0d3fcb5eb09087558a199fab
adamsl <eg1972@gmail.com>
2023-02-12 22:14:00 -0500

    rearrange code

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -256,9 +256,9 @@ namespace rgb_matrix
             y %= slab_height;
             if (y < panel_height) {
 
+                x += matrix_width / 2;
                 x = matrix_width - x - 1;
                 y = matrix_height - y - 1;
-                x += matrix_width / 2;
             
             } else {
                 x = visible_width - x - 1;
