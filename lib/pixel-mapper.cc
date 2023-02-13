e8b0744c973114a98e14aed8adfcf138499f0569
adamsl <eg1972@gmail.com>
2023-02-12 21:52:14 -0500

    back to normal

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -255,9 +255,9 @@ namespace rgb_matrix
             const int base_y = (y / slab_height) * panel_height;
             y %= slab_height;
             if (y < panel_height) {
-            // x += matrix_width / 2;
-            x = matrix_width - x - 1;
-            y = matrix_height - y - 1;
+            x += matrix_width / 2;
+            // x = matrix_width - x - 1;
+            // y = matrix_height - y - 1;
             } else {
             x = visible_width - x - 1;
             y = slab_height - y - 1;
