7ff973f9b6df350745736eaad190ed9e71705bd4
adamsl <eg1972@gmail.com>
2023-02-13 15:49:32 -0500

    defined ORIGINALLY_FOUR

--- lib/multiplex-mappers.cc
+++ lib/multiplex-mappers.cc
@@ -144,15 +144,16 @@ public:
   SuperbowlMultiplexMapper() : MultiplexMapperBase("Superbowl", 2) {}
 
   #define ORIGINALLY_TWO 2
+    #define ORIGINALLY_FOUR 4
   void MapSinglePanel(int x, int y, int *matrix_x, int *matrix_y) const {
-    const bool is_top_check = (y % (panel_rows_/ ORIGINALLY_TWO )) < panel_rows_/4;
+    const bool is_top_check = (y % (panel_rows_/ ORIGINALLY_TWO )) < panel_rows_/ ORIGINALLY_FOUR;
     const bool is_left_check = (x < panel_cols_/ ORIGINALLY_TWO );
     if (is_top_check) {
       *matrix_x = is_left_check ? x+panel_cols_/ ORIGINALLY_TWO  : x+panel_cols_;
     } else {
       *matrix_x = is_left_check ? x : x + panel_cols_/ ORIGINALLY_TWO;
     }
-    *matrix_y = ((y / (panel_rows_/ ORIGINALLY_TWO )) * (panel_rows_/4) + y % (panel_rows_/4));
+    *matrix_y = ((y / (panel_rows_/ ORIGINALLY_TWO )) * (panel_rows_/ ORIGINALLY_FOUR ) + y % (panel_rows_/ ORIGINALLY_FOUR ));
 
     printf( "panel_rows_: %d, panel_cols_: %d is_top_check: %d  is_left_check: %d  ", panel_rows_, panel_cols_, is_top_check, is_left_check );
     printf( "SuperbowlMultiplexMapper:  MapSinglePanel input x: %d, input: y: %d, matrix_x: %d, matrix_y: %d\n", x, y, *matrix_x, *matrix_y );             
