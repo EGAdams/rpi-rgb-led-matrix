7b44cdccc7bae18fc5455f7cdcd235c221054c42
adamsl <eg1972@gmail.com>
2023-02-13 12:44:19 -0500

    add in matrix_width / 2 at the bottom

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -258,9 +258,9 @@ namespace rgb_matrix
 
             if (y < panel_height) {
 
+                x = matrix_width  - x - 1;
+                y = matrix_height - y - 1;
                 x += matrix_width / 2;
-                // x = matrix_width  - x - 1;
-                //  y = matrix_height - y - 1;
             
             } else {
                 x = visible_width - x - 1;
