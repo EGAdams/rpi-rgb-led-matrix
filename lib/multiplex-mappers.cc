edaa5c5135f80df6859623699b9ba2d4981c12ae
adamsl <eg1972@gmail.com>
2023-02-13 15:02:14 -0500

    print columns and rows

--- lib/multiplex-mappers.cc
+++ lib/multiplex-mappers.cc
@@ -130,6 +130,7 @@ public:
     }
     *matrix_y = ((y / (panel_rows_/2)) * (panel_rows_/4)
                  + y % (panel_rows_/4));
+    printf( "panel_rows_: %d, panel_cols_: %d is_top_check: %d  is_left_check: %d", panel_rows_, panel_cols_, is_top_check, is_left_check );
     printf( "CheckeredMultiplexMapper:  MapSinglePanel input x: %d, input: y: %d, matrix_x: %d, matrix_y: %d\n", x, y, *matrix_x, *matrix_y );             
   }
 };
