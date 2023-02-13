bc89fa90233eca91c43990ec718ef913fab53773
adamsl <eg1972@gmail.com>
2023-02-13 13:07:24 -0500

    put visible width back!

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -252,7 +252,7 @@ namespace rgb_matrix
                                   int *matrix_x, int *matrix_y) const {
             printf( "incomming x: %d  incomming y: %d  ", x, y );
 
-            const int visible_width = (( MATRIX_WIDTH / 64) * 32 ) / 2;
+            const int visible_width = ( MATRIX_WIDTH / 64) * 32;
             const int base_y = (y / SLAB_HEIGHT) * MATRIX_HEIGHT;
             y %= SLAB_HEIGHT;
 
