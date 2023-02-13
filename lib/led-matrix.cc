74add97abbda4ea7b702fd6a29f2361e0a792dd6
adamsl <eg1972@gmail.com>
2023-02-12 22:37:26 -0500

    print

--- lib/led-matrix.cc
+++ lib/led-matrix.cc
@@ -579,7 +579,7 @@ bool RGBMatrix::Impl::ApplyPixelMapper(const PixelMapper *mapper) {
         // exit( 1 );        
         continue;
       } else {
-        //printf( "PixelMapper: ( %d, %d ) -> ( %d, %d ) [ range: %dx%d ]\n", x, y, orig_x, orig_y, old_width, old_height );
+        printf( "PixelMapper: ( %d, %d ) -> ( %d, %d ) [ range: %dx%d ]\n", x, y, orig_x, orig_y, old_width, old_height );
       }
       const internal::PixelDesignator *orig_designator;
       orig_designator = shared_pixel_mapper_->get(orig_x, orig_y);
