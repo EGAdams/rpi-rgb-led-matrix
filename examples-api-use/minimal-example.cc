d825eda3d1cbd0d7437923a8296e1a746e1d6a28
adamsl <eg1972@gmail.com>
2023-02-15 15:55:50 -0500

    adf

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
     defaults.cols = 32;
     defaults.brightness = 1;
     // defaults.pixel_mapper_config = "U-mapper";
-    defaults.multiplexing = 19; // 19 is the Superbowl mapper
+    // defaults.multiplexing = 19; // 19 is the Superbowl mapper
     defaults.show_refresh_rate = false;
     defaults.chain_length = 8;
     defaults.parallel = 1;
