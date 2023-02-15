136a1d7f1ccec6682c84b76cb7b1ef93a23c3c7c
adamsl <eg1972@gmail.com>
2023-02-15 15:53:06 -0500

    as

--- lib/led-matrix.cc
+++ lib/led-matrix.cc
@@ -582,7 +582,7 @@ bool RGBMatrix::Impl::ApplyPixelMapper(const PixelMapper *mapper) {
         exit( 1 );        
         //continue;
       } else {
-        if ( strcmp( mapper->GetName(),"U-mapper" )) {
+        if ( !strcmp( mapper->GetName(),"U-mapper" )) {
             printf( "PixelMapper: ( %d, %d ) -> ( %d, %d ) [ range: %dx%d ]\n", x, y, orig_x, orig_y, old_width, old_height );
         }
       }
