628b8de57d982b310ef81b5c9cd663fc5225335d
adamsl <eg1972@gmail.com>
2023-02-15 13:51:01 -0500

    apply wip..

--- lib/multiplex-mappers.cc
+++ lib/multiplex-mappers.cc
@@ -64,8 +64,14 @@ public:
 
     int new_x, new_y;
     MapSinglePanel(within_panel_x, within_panel_y, &new_x, &new_y); // map only one panel! in this case, ( 32x32 )
-    *matrix_x = chained_panel  * panel_stretch_factor_* panel_cols_ + new_x;
-    *matrix_y = parallel_panel * panel_rows_ / panel_stretch_factor_ + new_y;
+    if ( visible_y < 32 ) {
+        *matrix_x = chained_panel  * panel_stretch_factor_* panel_cols_ + new_x;
+        *matrix_y = parallel_panel * panel_rows_ / panel_stretch_factor_ + new_y;
+    } else {
+        *matrix_x = chained_panel  * panel_stretch_factor_* panel_cols_ + new_x + 64;
+        *matrix_y = parallel_panel * panel_rows_ / panel_stretch_factor_ + new_y - 32;
+    }
+    
     printf( "visible: ( %3d, %3d ) internally mapped: ( %3d, %2d ) chained_panel: %d  parallel_panel: %d  in_pnl_x: %2d  in_pnl_y: %2d \n", 
              visible_x, visible_y, *matrix_x, *matrix_y,           chained_panel, parallel_panel, within_panel_x, within_panel_y );
   }
