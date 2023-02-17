280cb7fa08ee39dde82e1ba5872f702191efcb20
adamsl <eg1972@gmail.com>
2023-02-17 17:48:30 -0500

    test

--- lib/multiplex-mappers.cc
+++ lib/multiplex-mappers.cc
@@ -20,73 +20,65 @@ namespace internal {
 // A Pixel Mapper maps physical pixels locations to the internal logical
 // mapping in a panel or panel-assembly, which depends on the wiring.
 class MultiplexMapperBase : public MultiplexMapper {
-public:
-  MultiplexMapperBase(const char *name, int stretch_factor)
-    : name_(name), panel_stretch_factor_(stretch_factor) {}
 
-  // This method is const, but we sneakily remember the original size
-  // of the panels so that we can more easily quantize things.
-  // So technically, we're stateful, but let's pretend we're not changing
-  // state. In the context this is used, it is never accessed in multiple
-  // threads.
-  virtual void EditColsRows(int *cols, int *rows) const {
-    panel_rows_ = *rows;
-    panel_cols_ = *cols;
+    public:
+    MultiplexMapperBase(const char *name, int stretch_factor)
+        : name_(name), panel_stretch_factor_(stretch_factor) {}
 
-    *rows /= panel_stretch_factor_;
-    *cols *= panel_stretch_factor_;
-    printf( "panel_stretch_factor: %d  panel_rows: %d  panel_cols: %d \n", panel_stretch_factor_, panel_rows_, panel_cols_ );
-  }
+    // This method is const, but we sneakily remember the original size
+    // of the panels so that we can more easily quantize things.
+    // So technically, we're stateful, but let's pretend we're not changing
+    // state. In the context this is used, it is never accessed in multiple
+    // threads.
+    virtual void EditColsRows(int *cols, int *rows) const {
+        panel_rows_ = *rows;
+        panel_cols_ = *cols;
 
-  virtual bool GetSizeMapping(int matrix_width, int matrix_height,
-                              int *visible_width, int *visible_height) const {
-    // Matrix width has been altered. Alter it back.
-    *visible_width = matrix_width / panel_stretch_factor_;
-    *visible_height = matrix_height * panel_stretch_factor_;
-    return true;
-  }
+        *rows /= panel_stretch_factor_;
+        *cols *= panel_stretch_factor_;
+    }
 
-  virtual const char *GetName() const { return name_; }
+    virtual bool GetSizeMapping(int matrix_width, int matrix_height,
+                                int *visible_width, int *visible_height) const {
+        // Matrix width has been altered. Alter it back.
+        *visible_width = matrix_width / panel_stretch_factor_;
+        *visible_height = matrix_height * panel_stretch_factor_;
+        return true;
+    }
 
-  // The MapVisibleToMatrix() as required by PanelMatrix here breaks it
-  // down to the individual panel, so that derived classes only need to
-  // implement MapSinglePanel().
-  
-  virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
-                                  int visible_x, int visible_y,
-                                  int *matrix_x, int *matrix_y) const {
-    const int chained_panel  = visible_x / panel_cols_;
-    const int parallel_panel = visible_y / panel_rows_;
+    virtual const char *GetName() const { return name_; }
 
-    const int within_panel_x = visible_x % panel_cols_;
-    const int within_panel_y = visible_y % panel_rows_;
-    
+    // The MapVisibleToMatrix() as required by PanelMatrix here breaks it
+    // down to the individual panel, so that derived classes only need to
+    // implement MapSinglePanel().
+    virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
+                                    int visible_x, int visible_y,
+                                    int *matrix_x, int *matrix_y) const {
+        const int chained_panel  = visible_x / panel_cols_;
+        const int parallel_panel = visible_y / panel_rows_;
 
-    int new_x, new_y;
-    MapSinglePanel( within_panel_x, within_panel_y, &new_x, &new_y ); // map only one panel! in this case, ( 32x32 )
-    // printf( "////////// done running MapSinglePanel() with ( %3d, %3d ) ////////// INSIDE BASE! //", within_panel_x, within_panel_y );
-   
-    *matrix_x = chained_panel  * panel_stretch_factor_* panel_cols_ + new_x;
-    *matrix_y = parallel_panel * panel_rows_ / panel_stretch_factor_ + new_y;
-    
-    //printf( "////////// visible: ( %3d, %3d ) internally mapped: ( %3d, %2d ) chained_panel: %d  parallel_panel: %d  in_pnl_x: %2d  in_pnl_y: %2d \n", 
-    //         visible_x, visible_y, *matrix_x, *matrix_y,           chained_panel, parallel_panel, within_panel_x, within_panel_y );
-  }
+        const int within_panel_x = visible_x % panel_cols_;
+        const int within_panel_y = visible_y % panel_rows_;
 
-  // Map the coordinates for a single panel. This is to be overridden in
-  // derived classes.
-  // Input parameter is the visible position on the matrix, and this method
-  // should return the internal multiplexed position.
-  virtual void MapSinglePanel(int visible_x, int visible_y,
-                              int *matrix_x, int *matrix_y) const = 0;
+        int new_x, new_y;
+        MapSinglePanel(within_panel_x, within_panel_y, &new_x, &new_y);
+        *matrix_x = chained_panel  * panel_stretch_factor_*panel_cols_ + new_x;
+        *matrix_y = parallel_panel * panel_rows_/panel_stretch_factor_ + new_y;
+    }
 
-protected:
-  const char *const name_;
-  const int panel_stretch_factor_;
-  static int count;
+    // Map the coordinates for a single panel. This is to be overridden in
+    // derived classes.
+    // Input parameter is the visible position on the matrix, and this method
+    // should return the internal multiplexed position.
+    virtual void MapSinglePanel(int visible_x, int visible_y,
+                                int *matrix_x, int *matrix_y) const = 0;
 
-  mutable int panel_cols_;
-  mutable int panel_rows_;
+    protected:
+    const char *const name_;
+    const int panel_stretch_factor_;
+
+    mutable int panel_cols_;
+    mutable int panel_rows_;
 };
 
 
