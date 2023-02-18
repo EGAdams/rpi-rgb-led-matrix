2cc672d6bf671a9e3491c0ea4ff5f4a3f46491c2
adamsl <eg1972@gmail.com>
2023-02-18 17:35:30 -0500

    test

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -383,10 +383,10 @@ namespace rgb_matrix
                 //                     y_offset + y,
                 //                     red, green, blue);
 
-                if ( y < 16 ) {
+                if ( y < 32 ) {
                     // x_offset = x + panel.x_offset;
                     // y_offset = y + panel.y_offset;
-                    y_offset = 32 - y;
+                    y_offset = 16 - y;
                 }
 
                 *matrix_x = x + x_offset;
