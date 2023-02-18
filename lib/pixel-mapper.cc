fc72db37746f5dda9e9a93e14651799d09dd097b
adamsl <eg1972@gmail.com>
2023-02-18 11:32:36 -0500

    cut rows in half.  now try 64 again.

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -293,7 +293,7 @@ namespace rgb_matrix
             #define PANEL_HEIGHT  32
             #define PANEL_WIDTH   32
             #define SLAB_HEIGHT   64
-            #define MATRIX_WIDTH  256
+            #define MATRIX_WIDTH  64
             #define MATRIX_HEIGHT 32
             #define VISIBLE_WIDTH 64
             #define CHAIN_LENGTH   16
