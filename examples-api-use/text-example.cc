19c9e551f2f0368d81fbc6985253106085e5eae7
adamsl <eg1972@gmail.com>
2023-02-21 21:09:15 -0500

    added periods and new fonts

--- examples-api-use/text-example.cc
+++ examples-api-use/text-example.cc
@@ -159,15 +159,25 @@ int main(int argc, char *argv[]) {
     // the outline font, we also fill the background here.
 
   Color firstRowColor( 0, 255, 0 );
-  rgb_matrix::DrawText(canvas, font, x, y + font.baseline(), firstRowColor, outline_font ? NULL : &bg_color, "30", letter_spacing );
+  rgb_matrix::Font period_font;
+  if (!period_font.LoadFont( "mspgothic_72_48" )) {
+    fprintf(stderr, "Couldn't load font '%s'\n", bdf_font_file);
+    return 1;
+  }
+  
+  Color period_color( 255, 255, 0 );
+  rgb_matrix::DrawText(canvas, period_font, x, y + font.baseline(), period_color,  outline_font ? NULL : &bg_color, ". ", letter_spacing );
+  rgb_matrix::DrawText(canvas, font,        x, y + font.baseline(), firstRowColor, outline_font ? NULL : &bg_color, "30", letter_spacing );
   y += font.height();
 
   Color secondRowColor( 255, 0, 0 );
-  rgb_matrix::DrawText(canvas, font, x, y + font.baseline(), secondRowColor, outline_font ? NULL : &bg_color, "20", letter_spacing );
+  rgb_matrix::DrawText(canvas, period_font, x, y + font.baseline(), period_color,   outline_font ? NULL : &bg_color, ". ", letter_spacing );
+  rgb_matrix::DrawText(canvas, font,        x, y + font.baseline(), secondRowColor, outline_font ? NULL : &bg_color, "20", letter_spacing );
   y += font.height();
 
   Color thirdRowColor( 0, 255, 0 );
-  rgb_matrix::DrawText(canvas, font, x, y + font.baseline(), thirdRowColor, outline_font ? NULL : &bg_color, "30", letter_spacing );
+  rgb_matrix::DrawText(canvas, period_font, x, y + font.baseline(), period_color,  outline_font ? NULL : &bg_color, ". ", letter_spacing );
+  rgb_matrix::DrawText(canvas,        font, x, y + font.baseline(), thirdRowColor, outline_font ? NULL : &bg_color, "30", letter_spacing );
   y += font.height();
 
   // initialize line buffer and // start reading from stdin
