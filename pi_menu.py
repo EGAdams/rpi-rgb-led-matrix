8c70404de102b932617bb1ca95b31b23adf82d89
adamsl <eg1972@gmail.com>
2023-03-06 13:33:39 -0500

    fix syntax error

--- pi_menu.py
+++ pi_menu.py
@@ -28,7 +28,7 @@ def main():
         os.system( "git pull" )
         main()
     elif choice == "3":
-        default_options = '--led-multiplexing=2 --led-brightness=1 --led-chain=8 --led-pixel-mapper="U-mapper="U-mapper" --led-gpio-mapping=adafruit-hat --led-brightness=15'
+        default_options = '--led-multiplexing=2 --led-brightness=1 --led-chain=8 --led-pixel-mapper="U-mapper" --led-gpio-mapping=adafruit-hat --led-brightness=15'
         print( "running 1st digit script... " )
         os.system( 'sudo examples-api-use/text-example -f fonts/mspgothic_70_32 ' + default_options )
         main()
