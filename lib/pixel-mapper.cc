6d752a5b183475aabc2074b24a51c747786d4879
adamsl <eg1972@gmail.com>
2023-03-18 19:34:56 -0400

    took out more original code

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -329,19 +329,19 @@ class TwoSixtyFourMapper : public PixelMapper {
             virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
                                   int x, int y,
                                   int *matrix_x, int *matrix_y) const {
-                const int panel_height = matrix_height / parallel_;
-                const int visible_width = (matrix_width / 64) * 32;
-                const int slab_height = 2 * panel_height;   // one folded u-shape
-                const int base_y = (y / slab_height) * panel_height;
-                y %= slab_height;
-                if (y < panel_height) {
-                x += matrix_width / 2;
-                } else {
-                x = visible_width - x - 1;
-                y = slab_height - y - 1;
-                }
-                *matrix_x = x;
-                *matrix_y = base_y + y;
+                // const int panel_height = matrix_height / parallel_;
+                // const int visible_width = (matrix_width / 64) * 32;
+                // const int slab_height = 2 * panel_height;   // one folded u-shape
+                // const int base_y = (y / slab_height) * panel_height;
+                // y %= slab_height;
+                // if (y < panel_height) {
+                // x += matrix_width / 2;
+                // } else {
+                // x = visible_width - x - 1;
+                // y = slab_height - y - 1;
+                // }
+                // *matrix_x = x;
+                // *matrix_y = base_y + y;
                 // Figure out what row and column panel this pixel is within.
                 #define PANEL_HEIGHT 32
                 int row = y / PANEL_HEIGHT; // _panel_height;
