93eca86755ce1d228d4537f7b90e869316521e95
adamsl <eg1972@gmail.com>
2023-03-29 15:47:25 -0400

    put original algorithm back, not the bot one.

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -262,8 +262,8 @@ class TwoSixtyFourMapper : public PixelMapper {
                 // int col = x / PANEL_WIDTH;
 
 
-                int panel_index = col * 2 + (y < PANEL_HEIGHT ? 0 : 1); // bot suggestion back in on tuesday after sleepy day.
-                // int panel_index = ( COLS * row ) + col;
+                // int panel_index = col * 2 + (y < PANEL_HEIGHT ? 0 : 1); // bot suggestion back in on tuesday after sleepy day.
+                int panel_index = ( COLS * row ) + col;
                 // printf( "row: %d  col: %d  panel index: %d\n", row, col,  panel_index );
                 
                 Panel panel = _panels[ panel_index ];  //_cols*row + col];
