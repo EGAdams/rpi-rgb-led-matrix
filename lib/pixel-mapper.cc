055cc52e50bfa00a58a633b7903bc23f43a70ae8
adamsl <eg1972@gmail.com>
2023-03-30 16:13:44 -0400

    panel height to 64

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -235,7 +235,7 @@ class OneSixtyFourMapper : public PixelMapper {
             #define ROWS           4
             #define COLS           2
             #define PANEL_PARALLEL 0
-            #define PANEL_HEIGHT   16 // 32 // 64
+            #define PANEL_HEIGHT   64
 
             virtual bool GetSizeMapping(int matrix_width, int matrix_height,
                               int *visible_width, int *visible_height)
