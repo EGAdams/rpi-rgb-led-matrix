bc37221bf18ac42409196b45afcde9934bb0fab3
adamsl <eg1972@gmail.com>
2023-03-31 20:01:44 -0400

    test

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -209,11 +209,7 @@ class OneSixtyFourMapper : public PixelMapper {
             // These transformations are explained in detail in https://github.com/hzeller/rpi-rgb-led-matrix/blob/master/lib/transformer.cc
             // If the physical pixel is in the upper half of the panel
             if (y < ( kPanelHeight / 2 )) {
-                // If the physical pixel is in an odd column
-                // if (x & 0x01) {
-                // // Mirror the x coordinate
-                // out_x = kPanelWidth - 1 - x;
-                // }
+                out_y += kPanelHeight / 2;
             }
             // If the physical pixel is in the lower half of the panel
             else {
