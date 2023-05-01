1522637bdfa8a846ad083d0517321b0856f0ad9f
adamsl <eg1972@gmail.com>
2023-05-01 12:44:43 -0400

    space and pixel tweaking

--- examples-api-use/text-example.cc
+++ examples-api-use/text-example.cc
@@ -172,10 +172,11 @@ int main(int argc, char *argv[]) {
     fprintf( stderr, "Couldn't load font '%s'\n", BIG_NUMBER_FONT );
     return 1; }
   
-  #define PERIOD_SPACING 14
-  #define BEGINNING_SPACE 3
+  #define PERIOD_SPACING 16
+  #define SPACE_BEFORE_2ND_NUMBER 4
+  #define PIPE_SPACE 1
   Color pipe_color( 255, 255, 0 );
-  rgb_matrix::DrawText(canvas, big_number_font, x + BEGINNING_SPACE, y + big_number_font.baseline(), pipe_color,  outline_font ? NULL : &bg_color, "I", letter_spacing );
+  rgb_matrix::DrawText(canvas, big_number_font, x + PIPE_SPACE, y + big_number_font.baseline(), pipe_color,  outline_font ? NULL : &bg_color, "I", letter_spacing );
   
   rgb_matrix::DrawText(canvas, big_number_font, x + PERIOD_SPACING, y + big_number_font.baseline(), firstRowColor, outline_font ? NULL : &bg_color, "15", letter_spacing );
   y += big_number_font.height();
@@ -183,9 +184,9 @@ int main(int argc, char *argv[]) {
   printf("Font height before second row: %d\n", big_number_font.height());
 
   Color secondRowColor( 255, 0, 0 );
-  rgb_matrix::DrawText(canvas, big_number_font, x + BEGINNING_SPACE, y + big_number_font.baseline(), secondRowColor, outline_font ? NULL : &bg_color, " 30", letter_spacing );
+  rgb_matrix::DrawText(canvas, big_number_font, x + SPACE_BEFORE_2ND_NUMBER, y + big_number_font.baseline(), secondRowColor, outline_font ? NULL : &bg_color, " 30", letter_spacing );
   
-  y += big_number_font.height();
+  y += big_number_font.height() + 4;
   printf("Font height before third row: %d\n", big_number_font.height());
 
   #define LITTLE_NUMBER_FONT "fonts/little_numbers.bdf"
@@ -195,13 +196,13 @@ int main(int argc, char *argv[]) {
     return 1; }
 
   Color thirdRowColor( 0, 255, 0 );
-  rgb_matrix::DrawText(canvas, little_number_font, x + BEGINNING_SPACE, y + little_number_font.baseline(), thirdRowColor,  outline_font ? NULL : &bg_color, "6 4 4", letter_spacing );
+  rgb_matrix::DrawText(canvas, little_number_font, x + SPACE_BEFORE_2ND_NUMBER, y + little_number_font.baseline(), thirdRowColor,  outline_font ? NULL : &bg_color, "6 4 4", letter_spacing );
 
-  y += little_number_font.height();
+  y += little_number_font.height() + 3;
   printf("Font height before fourth row: %d\n", little_number_font.height());
 
   Color fourthRowColor( 255, 0, 0 );
-  rgb_matrix::DrawText(canvas, little_number_font, x + BEGINNING_SPACE, y + little_number_font.baseline(), fourthRowColor, outline_font ? NULL : &bg_color, "3 6 1", letter_spacing );
+  rgb_matrix::DrawText(canvas, little_number_font, x + SPACE_BEFORE_2ND_NUMBER, y + little_number_font.baseline(), fourthRowColor, outline_font ? NULL : &bg_color, "3 6 1", letter_spacing );
   
   // initialize line buffer and // start reading from stdin
   char line[1024];
