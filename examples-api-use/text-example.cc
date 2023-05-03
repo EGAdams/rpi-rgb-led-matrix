ee487c3f7db337b09cada870b9a776357c6f8c69
adamsl <eg1972@gmail.com>
2023-05-03 10:33:31 -0400

    remove 1 row from the top

--- examples-api-use/text-example.cc
+++ examples-api-use/text-example.cc
@@ -182,8 +182,8 @@ int main(int argc, char *argv[]) {
   Color pipe_color( 255, 255, 0 );
   rgb_matrix::DrawText(canvas, big_number_font, x + PIPE_SPACE, y + big_number_font.baseline(), pipe_color,  outline_font ? NULL : &bg_color, "I", letter_spacing );
   
-  rgb_matrix::DrawText(canvas, big_number_font, x + SPACE_BEFORE_1ST_NUMBER, y + big_number_font.baseline(), firstRowColor, outline_font ? NULL : &bg_color, "1", LETTER_SPACING );
-  rgb_matrix::DrawText(canvas, big_number_font, x + COORDS_FOR_SECOND_NUMBER, y + big_number_font.baseline(), firstRowColor, outline_font ? NULL : &bg_color, "5", LETTER_SPACING );
+  rgb_matrix::DrawText(canvas, big_number_font, x + SPACE_BEFORE_1ST_NUMBER, y + big_number_font.baseline() - 1, firstRowColor, outline_font ? NULL : &bg_color, "1", LETTER_SPACING );
+  rgb_matrix::DrawText(canvas, big_number_font, x + COORDS_FOR_SECOND_NUMBER, y + big_number_font.baseline() - 1, firstRowColor, outline_font ? NULL : &bg_color, "5", LETTER_SPACING );
   
   y += big_number_font.height();
   // print font height
