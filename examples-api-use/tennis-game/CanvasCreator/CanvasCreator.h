92514ea6f4db877e92163d23c3569dc63a612eb0
adamsl <eg1972@gmail.com>
2023-05-17 12:02:56 -0400

    added new classes to help update the scoreboard.

---
+++ examples-api-use/tennis-game/CanvasCreator/CanvasCreator.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <led-matrix.h>
+
+class CanvasCreator {
+public:
+    CanvasCreator(RGBMatrix::Options matrix_options, rgb_matrix::RuntimeOptions runtime_opt);
+
+    RGBMatrix* CreateCanvas();
+
+private:
+    RGBMatrix::Options _matrixOptions;
+    rgb_matrix::RuntimeOptions _runtimeOptions;
+};
