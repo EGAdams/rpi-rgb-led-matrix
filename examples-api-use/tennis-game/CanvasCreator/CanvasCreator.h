9361e0b8fe436fa8a0efa701485424b5ca9d69a7
adamsl <eg1972@gmail.com>
2023-05-17 13:36:29 -0400

    added bot suggested rules to make file

--- examples-api-use/tennis-game/CanvasCreator/CanvasCreator.h
+++ examples-api-use/tennis-game/CanvasCreator/CanvasCreator.h
@@ -1,16 +1,10 @@
 #pragma once
-
 #include "led-matrix.h"
-
 using namespace rgb_matrix;
-
 class CanvasCreator {
 public:
     CanvasCreator(RGBMatrix::Options matrix_options, rgb_matrix::RuntimeOptions runtime_opt);
-
     RGBMatrix* CreateCanvas();
-
 private:
     RGBMatrix::Options _matrixOptions;
-    rgb_matrix::RuntimeOptions _runtimeOptions;
-};
+    rgb_matrix::RuntimeOptions _runtimeOptions;};
