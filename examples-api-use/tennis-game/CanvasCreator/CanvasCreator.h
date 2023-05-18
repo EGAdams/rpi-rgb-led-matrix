5e5a6466286e9985dfdae99bda0bd418a3fce04b
adamsl <eg1972@gmail.com>
2023-05-18 14:03:44 -0400

    remove bracket and replace with "?

--- examples-api-use/tennis-game/CanvasCreator/CanvasCreator.h
+++ examples-api-use/tennis-game/CanvasCreator/CanvasCreator.h
@@ -1,12 +1,13 @@
 #pragma once
-#include <led-matrix.h>
+#include "led-matrix.h"
 
 using namespace rgb_matrix;
+
 class CanvasCreator {
 public:
     CanvasCreator(RGBMatrix::Options matrix_options, rgb_matrix::RuntimeOptions runtime_opt);
     RGBMatrix* CreateCanvas();
-    
+
 private:
     RGBMatrix::Options         _matrixOptions;
     rgb_matrix::RuntimeOptions _runtimeOptions;};
