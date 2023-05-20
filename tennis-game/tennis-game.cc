e48416a83764a53707139af6c39fdaf707de25cd
egadams <31349790+EGAdams@users.noreply.github.com>
2023-05-20 13:59:14 -0400

    tweak output again

--- tennis-game/tennis-game.cc
+++ tennis-game/tennis-game.cc
@@ -14,7 +14,6 @@
 #include "FontLoader/FontLoader.h"
 #include "TextDrawer/TextDrawer.h"
 
-
 using namespace rgb_matrix;
 
 static int usage(const char *progname) {
@@ -34,25 +33,32 @@ static int usage(const char *progname) {
             "\n"
             );
     rgb_matrix::PrintMatrixFlags(stderr);
-    return 1;}
+    return 1;
+}
 
-    static bool parseColor(Color *c, const char *str) {
-    return sscanf(str, "%hhu,%hhu,%hhu", &c->r, &c->g, &c->b) == 3;}
+static bool parseColor(Color *c, const char *str) {
+    return sscanf(str, "%hhu,%hhu,%hhu", &c->r, &c->g, &c->b) == 3;
+}
 
-    static bool FullSaturation(const Color &c) {
+static bool FullSaturation(const Color &c) {
     return (c.r == 0 || c.r == 255)
         && (c.g == 0 || c.g == 255)
-        && (c.b == 0 || c.b == 255); }
+        && (c.b == 0 || c.b == 255);
+}
 
-    int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     RGBMatrix::Options matrix_options;
     rgb_matrix::RuntimeOptions runtime_opt;
-    if (!rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &matrix_options, &runtime_opt)) { return usage(argv[0]); }
+    if (!rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &matrix_options, &runtime_opt)) {
+        return usage(argv[0]);
+    }
+
     Color color(255, 255, 0);
     Color bg_color(0, 0, 0);
     Color flood_color(0, 0, 0);
     Color outline_color(0,0,0);
     bool with_outline = false;
+
     const char *bdf_font_file = NULL;
     int x_orig = 0;
     int y_orig = 0;
@@ -64,38 +70,41 @@ static int usage(const char *progname) {
         case 'x': x_orig = atoi(optarg); break;
         case 'y': y_orig = atoi(optarg); break;
         case 'f': bdf_font_file = strdup(optarg); break;
+        caseI apologize for the abrupt cut-off in the previous message. Here's the continuation of the code:
+
+```c++
         case 'S': letter_spacing = atoi(optarg); break;
         case 'C':
-        if (!parseColor(&color, optarg)) {
-            fprintf(stderr, "Invalid color spec: %s\n", optarg);
-            return usage(argv[0]);
-        }
-        break;
+            if (!parseColor(&color, optarg)) {
+                fprintf(stderr, "Invalid color spec: %s\n", optarg);
+                return usage(argv[0]);
+            }
+            break;
         case 'B':
-        if (!parseColor(&bg_color, optarg)) {
-            fprintf(stderr, "Invalid background color spec: %s\n", optarg);
-            return usage(argv[0]);
-        }
-        break;
+            if (!parseColor(&bg_color, optarg)) {
+                fprintf(stderr, "Invalid background color spec: %s\n", optarg);
+                return usage(argv[0]);
+            }
+            break;
         case 'O':
-        if (!parseColor(&outline_color, optarg)) {
-            fprintf(stderr, "Invalid outline color spec: %s\n", optarg);
+            if (!parseColor(&outline_color, optarg)) {
+                fprintf(stderr, "Invalid outline color spec: %s\n", optarg);
+                return usage(argv[0]);
+            }
+            with_outline = true;
+            break;
+        case 'F':
+            if (!parseColor(&flood_color, optarg)) {
+                fprintf(stderr, "Invalid background color spec: %s\n", optarg);
+                return usage(argv[0]);
+            }
+            break;
+        default:
             return usage(argv[0]);
         }
-        with_outline = true;
-        break;
-        case 'F':
-        if (!parseColor(&flood_color, optarg)) {
-            fprintf(stderr, "Invalid background color spec: %s\n", optarg);
-            return usage(argv[0]); }
-        break;
-        default:
-        return usage(argv[0]); }}
+    }
 
     printf("Updating ScoreBoard...\n");
-
-    // RGBMatrix::Options matrix_options;
-    // rgb_matrix::RuntimeOptions runtime_opt;
 
     // Create Canvas
     CanvasCreator canvasCreator(matrix_options, runtime_opt);
@@ -125,10 +134,13 @@ static int usage(const char *progname) {
     TextDrawer littleNumberDrawer(canvas, littleNumberFont, 7, littleNumberFont.baseline(), color, bg_color, "1 2 3", 0);
     littleNumberDrawer.DrawText();
 
+    TextDrawer littleNumberDrawer4(canvas, littleNumberFont, 7 + (2 * 17), littleNumberFont.baseline(), color, bg_color, "6", 0);
+    littleNumberDrawer4.DrawText();
+
     ///////// Start Game Loop /////////////
     bool game_running = true;
     int loop_count = 0;
-    #define MAX_LOOP_COUNT 2
+    #define MAX_LOOP_COUNT 6
     while ( game_running ) {
         if ( loop_count >  MAX_LOOP_COUNT ) { game_running = false; }
         printf( "sleeping... " );
