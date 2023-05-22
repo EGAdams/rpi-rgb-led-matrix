ab7a074e4e2cb05f2e460c6552df734ba7fffcb1
EG Adams <eg1972@gmail.com>
2023-05-22 11:46:28 -0400

    try to fix NumberDrawer errors

--- tennis-game/NumberDrawer/NumberDrawer.cpp
+++ tennis-game/NumberDrawer/NumberDrawer.cpp
@@ -1,21 +1,8 @@
-
 #include "NumberDrawer.h"
 
-class NumberDrawer {
-public:
-    enum Size { BIG, SMALL };
+NumberDrawer::NumberDrawer(RGBMatrix* canvas, const rgb_matrix::Font& font, Size size, const Color& color, const Color& bg_color)
+    : canvas_(canvas), font_(font), size_(size), color_(color), bg_color_(bg_color) {}
 
-    NumberDrawer(RGBMatrix* canvas, const rgb_matrix::Font& font, Size size, const Color& color, const Color& bg_color)
-        : canvas_(canvas), font_(font), size_(size), color_(color), bg_color_(bg_color) {}
-
-    void DrawNumber(const std::string& number, int x, int y) {
-        rgb_matrix::DrawText(canvas_, font_, x, y, color_, &bg_color_, number.c_str(), 0);
-    }
-
-private:
-    RGBMatrix* canvas_;
-    rgb_matrix::Font font_;
-    Size size_;
-    Color color_;
-    Color bg_color_;
-};
+void NumberDrawer::DrawNumber(const std::string& number, int x, int y) {
+    rgb_matrix::DrawText(canvas_, font_, x, y, color_, &bg_color_, number.c_str(), 0);
+}
