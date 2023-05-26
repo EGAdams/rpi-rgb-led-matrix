45de4d78194c1147912c745709689567355f5214
EG Adams <eg1972@gmail.com>
2023-05-26 10:06:43 -0400

    fixed logger

--- tennis-game/Logger/Logger.h
+++ tennis-game/Logger/Logger.h
@@ -4,10 +4,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include "GameTimer.h"
-#include "DIGI_V6_15.h"
-
-#if defined _WIN32 || defined _WIN64
+#include "../GameTimer/GameTimer.h"
 
 class Logger {
 public:
@@ -23,13 +20,4 @@ private:
     std::ofstream _log_file;
     std::vector<int> _used_random_numbers;
 };
-#else
-class Logger {
-public:
-    Logger( String path );
-    ~Logger();
-    void logUpdate( String message, String caller );
-};
-#endif  // defined _WIN*
-
 #endif  // Logger_h
