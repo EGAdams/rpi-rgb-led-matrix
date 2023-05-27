4e69b569aee38b00b7cd8c96f30824dfc58f835f
EG Adams <eg1972@gmail.com>
2023-05-27 13:59:39 -0400

    remove matrix from board

--- tennis-game/ScoreBoard/ScoreBoard.h
+++ tennis-game/ScoreBoard/ScoreBoard.h
@@ -4,8 +4,6 @@
 #include "../Player/Player.h"
 #include "../GameState/GameState.h"
 
-using namespace rgb_matrix;
-
 class ScoreBoard {
   public:
       ScoreBoard( Player* player1, Player* player2, GameState* gameState );
