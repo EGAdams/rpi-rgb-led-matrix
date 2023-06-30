2d14e2c0607af76fed06a147ce4119e356d646fb
adamsl <eg1972@gmail.com>
2023-06-30 10:47:37 -0400

    set history text makes good.

--- tennis-game/SetHistoryText/SetHistoryText.cpp
+++ tennis-game/SetHistoryText/SetHistoryText.cpp
@@ -5,13 +5,12 @@ SetHistoryText::~SetHistoryText() { std::cout << "destructing SetHistoryText..."
 
 std::string SetHistoryText::getSetHistoryText( int player ) {  
     std::string setHistoryText = _buildString( player );
-    std::cout << "setHistoryText: " << setHistoryText << std::endl;
-}
+    return setHistoryText; }
 
-std::string _buildString( int player ) {
-    std::string setHistoryText = "";
-    for ( int i = 0; i < _gameState->getSetHistory( player ).size(); i++ ) {
-        setHistoryText += std::to_string( _gameState->getSetHistory( player )[ i ] );
+std::string SetHistoryText::_buildString( int player ) {
+    std::map< int, int > setHistory = player == 1 ? _gameState->getPlayer1SetHistory() : _gameState->getPlayer2SetHistory();
+    std::string setHistoryText = ""; 
+    for ( int i = 0; i < setHistory.size(); i++ ) {
+        setHistoryText += std::to_string( setHistory[ i ] );
         setHistoryText += " "; }
-    return setHistoryText; 
-}
+    return setHistoryText; }
