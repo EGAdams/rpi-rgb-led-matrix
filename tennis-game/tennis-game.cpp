ec2cf9dc8d2ce5fb6c4f7db293312aedabfc4eb4
egadams <eg1972@gmail.com>
2023-08-24 11:35:12 -0400

    commit AND sync

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -68,6 +68,7 @@ int main() {
         // Check if the line starts a new test
         if ( line.find( "## Test " ) != std::string::npos ) {
             std::cout << "Test " << test_count << std::endl;
+            
             scoreBoard->writeMessage( "Test " + std::to_string( test_count ));
             // print the line
             // replace "## Test " with "Test "
