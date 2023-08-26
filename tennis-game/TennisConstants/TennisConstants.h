fe1cf64eb64a8abdac084659e60178250a5c256b
egadams <eg1972@gmail.com>
2023-08-26 16:18:05 -0400

    enable matrix

--- tennis-game/TennisConstants/TennisConstants.h
+++ tennis-game/TennisConstants/TennisConstants.h
@@ -34,7 +34,7 @@
 #define SCORE_CASE_4             4
 #define SCORE_CASE_5             5
 #define UNDEFINED_SCORE          99
-#define MATRIX_DISABLED          1
+#define MATRIX_DISABLED          0
 
 // #define INPUT 0 // not in windows.. ouch!!
 // #define OUTPUT 0
