d9d4a87de69bc68ac9de7b858da60af23895e503
egadams <eg1972@gmail.com>
2023-08-28 18:19:49 -0400

    fixed make error

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -19,6 +19,25 @@
 #include "GameObject/GameObject.h"
 
 using namespace rgb_matrix;
+#define SCORE_DELAY    .15
+
+void score( GameObject* gameObject, GameState* gameState, int player, int* loop_count ) {
+    std::cout << "\n\n\n\n\n\n\n*** Player " << player << " scored ***\n" << std::endl;
+    gameObject->playerScore( player );  // flip the player score flag
+    sleep( SCORE_DELAY );
+    gameObject->loopGame();  // handle the player score flag
+    loop_count++;
+    std::cout << "player 1 points: " << gameState->getPlayer1Points();
+    std::cout << "  player 2 points: " << gameState->getPlayer2Points() << std::endl;
+    std::cout << "player 1 games:  "  << gameState->getPlayer1Games();
+    std::cout << "  player 2 games:  "  << gameState->getPlayer2Games()  << std::endl;
+    std::cout << "player 1 sets:   "   << gameState->getPlayer1Sets();
+    std::cout << "  player 2 sets:   "   << gameState->getPlayer2Sets();
+    std::cout << "     current set: "     << gameState->getCurrentSet()      << std::endl;
+    std::map<int, int> _player1_set_history = gameState->getPlayer1SetHistory();
+    std::map<int, int> _player2_set_history = gameState->getPlayer2SetHistory();
+    std::cout << "end of game loop.  loop_count: " << loop_count << std::endl;
+}
 
 int main( int argc, char *argv[]) {
     int loop_count = 0;
@@ -44,7 +63,8 @@ int main( int argc, char *argv[]) {
     std::cout << "done calling loopGame().  sleeping...\n\n\n\n\n" << std::endl;
     sleep( 1 );
 
-    int player = 1;
+
+
     std::signal( SIGINT, GameObject::_signalHandler );
     /*/// Begin Game Loop ///*/ while ( gameState->gameRunning() && GameObject::gSignalStatus != SIGINT ) { 
         if ( loop_count >  MAX_LOOP_COUNT ) { gameState->stopGameRunning(); }
@@ -94,23 +114,4 @@ int main( int argc, char *argv[]) {
         std::cout << "MAX_LOOP_COUNT reached.  Exiting...\n\n\n\n\n" << std::endl; }
     return 0; 
 }
-
-void score( GameObject* gameObject, GameState* gameState, int player, int* loop_count ) {
-    std::cout << "\n\n\n\n\n\n\n*** Player " << player << " scored ***\n" << std::endl;
-    gameObject->playerScore( player );  // flip the player score flag
-    sleep( SCORE_DELAY );
-    gameObject->loopGame();  // handle the player score flag
-    loop_count++;
-    std::cout << "player 1 points: " << gameState->getPlayer1Points();
-    std::cout << "  player 2 points: " << gameState->getPlayer2Points() << std::endl;
-    std::cout << "player 1 games:  "  << gameState->getPlayer1Games();
-    std::cout << "  player 2 games:  "  << gameState->getPlayer2Games()  << std::endl;
-    std::cout << "player 1 sets:   "   << gameState->getPlayer1Sets();
-    std::cout << "  player 2 sets:   "   << gameState->getPlayer2Sets();
-    std::cout << "     current set: "     << gameState->getCurrentSet()      << std::endl;
-    std::map<int, int> _player1_set_history = gameState->getPlayer1SetHistory();
-    std::map<int, int> _player2_set_history = gameState->getPlayer2SetHistory();
-    std::cout << "end of game loop.  loop_count: " << loop_count << std::endl;
-}
-
     
