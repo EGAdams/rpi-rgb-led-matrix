82dc1a4e8226206100a6560839496d7a7a95fc5f
egadams <eg1972@gmail.com>
2023-08-28 18:17:44 -0400

    added score()

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -52,41 +52,39 @@ int main( int argc, char *argv[]) {
         
         // loop 6 times
         for ( int for_loop_count = 0; for_loop_count < 3; for_loop_count++ ) {
-            player = 1;
-            std::cout << "\n\n\n\n\n\n\n*** Player " << player << " scored ***\n" << std::endl;
-            gameObject->playerScore( player );  // flip the player score flag
+            score( gameObject, gameState, 1, &loop_count );
+            score( gameObject, gameState, 1, &loop_count );
             sleep( SCORE_DELAY );
-            gameObject->loopGame();  // handle the player score flag
-            loop_count++;
-            std::cout << "player 1 points: " << gameState->getPlayer1Points();
-            std::cout << "  player 2 points: " << gameState->getPlayer2Points() << std::endl;
-            std::cout << "player 1 games:  "  << gameState->getPlayer1Games();
-            std::cout << "  player 2 games:  "  << gameState->getPlayer2Games()  << std::endl;
-            std::cout << "player 1 sets:   "   << gameState->getPlayer1Sets();
-            std::cout << "  player 2 sets:   "   << gameState->getPlayer2Sets();
-            std::cout << "     current set: "     << gameState->getCurrentSet()      << std::endl;
-            std::map<int, int> _player1_set_history = gameState->getPlayer1SetHistory();
-            std::map<int, int> _player2_set_history = gameState->getPlayer2SetHistory();
-            std::cout << "end of game loop.  loop_count: " << loop_count << std::endl;
-            player = 2;
-            std::cout << "\n\n\n\n\n\n\n*** Player " << player << " scored ***\n" << std::endl;
-            gameObject->playerScore( player );  // flip the player score flag
+            score( gameObject, gameState, 2, &loop_count );
+            score( gameObject, gameState, 2, &loop_count );
             sleep( SCORE_DELAY );
-            gameObject->loopGame();  // handle the player score flag
-            loop_count++;
-            std::cout << "player 1 points: " << gameState->getPlayer1Points();
-            std::cout << "  player 2 points: " << gameState->getPlayer2Points() << std::endl;
-            std::cout << "player 1 games:  "  << gameState->getPlayer1Games();
-            std::cout << "  player 2 games:  "  << gameState->getPlayer2Games()  << std::endl;
-            std::cout << "player 1 sets:   "   << gameState->getPlayer1Sets();
-            std::cout << "  player 2 sets:   "   << gameState->getPlayer2Sets();
-            std::cout << "     current set: "     << gameState->getCurrentSet()      << std::endl;
-            _player1_set_history = gameState->getPlayer1SetHistory();
-            _player2_set_history = gameState->getPlayer2SetHistory();
-            std::cout << "end of game loop.  loop_count: " << loop_count << std::endl;
+            score( gameObject, gameState, 1, &loop_count );
+            score( gameObject, gameState, 1, &loop_count );
+            sleep( SCORE_DELAY );
+            score( gameObject, gameState, 2, &loop_count );
+            score( gameObject, gameState, 2, &loop_count );
+            sleep( SCORE_DELAY );
+            score( gameObject, gameState, 1, &loop_count );
+            score( gameObject, gameState, 1, &loop_count );
+            sleep( SCORE_DELAY );
+            // player = 2;
+            // std::cout << "\n\n\n\n\n\n\n*** Player " << player << " scored ***\n" << std::endl;
+            // gameObject->playerScore( player );  // flip the player score flag
+            // sleep( SCORE_DELAY );
+            // gameObject->loopGame();  // handle the player score flag
+            // loop_count++;
+            // std::cout << "player 1 points: " << gameState->getPlayer1Points();
+            // std::cout << "  player 2 points: " << gameState->getPlayer2Points() << std::endl;
+            // std::cout << "player 1 games:  "  << gameState->getPlayer1Games();
+            // std::cout << "  player 2 games:  "  << gameState->getPlayer2Games()  << std::endl;
+            // std::cout << "player 1 sets:   "   << gameState->getPlayer1Sets();
+            // std::cout << "  player 2 sets:   "   << gameState->getPlayer2Sets();
+            // std::cout << "     current set: "     << gameState->getCurrentSet()      << std::endl;
+            // _player1_set_history = gameState->getPlayer1SetHistory();
+            // _player2_set_history = gameState->getPlayer2SetHistory();
+            //std::cout << "end of game loop.  loop_count: " << loop_count << std::endl;
         }
-        std::cout << "\n\nenter 1 or 2 to score for player 1 or 2: ";
-        std::cin >> player;
+        sleep( 3 );
     } ///////// End Game Loop /////////
     std::cout << "game loop exited.  loop_count: " << loop_count << std::endl;
     if ( loop_count > MAX_LOOP_COUNT ) {
@@ -94,6 +92,25 @@ int main( int argc, char *argv[]) {
         std::cout << "sleeping for 5 seconds..." << std::endl;
         sleep( 120 );
         std::cout << "MAX_LOOP_COUNT reached.  Exiting...\n\n\n\n\n" << std::endl; }
-    return 0; }
+    return 0; 
+}
+
+void score( GameObject* gameObject, GameState* gameState, int player, int* loop_count ) {
+    std::cout << "\n\n\n\n\n\n\n*** Player " << player << " scored ***\n" << std::endl;
+    gameObject->playerScore( player );  // flip the player score flag
+    sleep( SCORE_DELAY );
+    gameObject->loopGame();  // handle the player score flag
+    loop_count++;
+    std::cout << "player 1 points: " << gameState->getPlayer1Points();
+    std::cout << "  player 2 points: " << gameState->getPlayer2Points() << std::endl;
+    std::cout << "player 1 games:  "  << gameState->getPlayer1Games();
+    std::cout << "  player 2 games:  "  << gameState->getPlayer2Games()  << std::endl;
+    std::cout << "player 1 sets:   "   << gameState->getPlayer1Sets();
+    std::cout << "  player 2 sets:   "   << gameState->getPlayer2Sets();
+    std::cout << "     current set: "     << gameState->getCurrentSet()      << std::endl;
+    std::map<int, int> _player1_set_history = gameState->getPlayer1SetHistory();
+    std::map<int, int> _player2_set_history = gameState->getPlayer2SetHistory();
+    std::cout << "end of game loop.  loop_count: " << loop_count << std::endl;
+}
 
     
