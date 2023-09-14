ba68e0ec158f40c4f026027622fa5b39bc1b6273
egadams <eg1972@gmail.com>
2023-09-14 10:52:00 -0400

    finish player2 win

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -123,6 +123,7 @@ void test_03( GameObject* gameObject, GameState* gameState, int* loop_count ) {
     playerWin( gameObject, gameState, 2 );
     playerWin( gameObject, gameState, 2 );
     playerWin( gameObject, gameState, 2 );
+    score( gameObject, gameState, 1     );  // player 2 win?
     std::cout << " ready for player 1 set win..." << std::endl;
     // playerWin( gameObject, gameState, 1 );
     std::cout << "player 1 won the set!" << std::endl;
