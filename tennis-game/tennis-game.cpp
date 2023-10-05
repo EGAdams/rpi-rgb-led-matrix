1335d1c2026f79b649073e9d47930171c18adeee
egadams <31349790+EGAdams@users.noreply.github.com>
2023-10-05 13:12:26 -0400

    adjust again.  tie-break over too soon.

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -204,7 +204,6 @@ void test_05( GameObject* gameObject, GameState* gameState, int* loop_count ) {
     score( gameObject, gameState, 1 );
     score( gameObject, gameState, 1 );
     score( gameObject, gameState, 1 );
-    score( gameObject, gameState, 1 );
     score( gameObject, gameState, 2 );
     score( gameObject, gameState, 1 );
     score( gameObject, gameState, 2 );
