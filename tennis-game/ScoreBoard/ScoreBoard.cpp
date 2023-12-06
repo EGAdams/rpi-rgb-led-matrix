3512bab879138a0a1d593b4c3e87424ecc369174
egadams <eg1972@gmail.com>
2023-12-06 14:04:07 -0500

    Fix player score blinking bug

--- tennis-game/ScoreBoard/ScoreBoard.cpp
+++ tennis-game/ScoreBoard/ScoreBoard.cpp
@@ -65,8 +65,8 @@ ScoreBoard::ScoreBoard( Player* player1, Player* player2, GameState* gameState )
         _drawer          = std::make_unique<Drawer>(    _canvas.get(), &_big_number_font, Drawer::SMALL, color, bg_color     );
         _pipeDrawer      = std::make_unique<Drawer>(    _canvas.get(), &_big_number_font, Drawer::BIG, color, bg_color       );
         _bluePipeDrawer  = std::make_unique<Drawer>(    _canvas.get(), &_big_number_font, Drawer::BIG, blue_color, bg_color  );
-        _redPipeDrawer   = std::make_unique<Drawer>(    _canvas.get(), &_big_number_font, Drawer::BIG, red_color, bg_color   );
-        _greenPipeDrawer = std::make_unique<Drawer>(    _canvas.get(), &_big_number_font, Drawer::BIG, green_color, bg_color );
+        // _redPipeDrawer   = std::make_unique<Drawer>(    _canvas.get(), &_big_number_font, Drawer::BIG, red_color, bg_color   );
+        // _greenPipeDrawer = std::make_unique<Drawer>(    _canvas.get(), &_big_number_font, Drawer::BIG, green_color, bg_color );
         _setDrawer       = std::make_unique<SetDrawer>( _canvas.get(), _gameState                                            );
         } // fi onRaspberryPi
     update();
@@ -171,16 +171,16 @@ void ScoreBoard::_drawTieBreakerBar() {
 
 void ScoreBoard::blink_player_score(int player) {
     // if ( !onRaspberryPi()) { return; }
-    if (player == PLAYER_1_SCORE) {
-        // _greenPipeDrawer->drawNumber("I", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
-        // std::this_thread::sleep_for(std::chrono::seconds(1)); // sleep 1 second
-        // _greenPipeDrawer->drawNumber("I", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
-        // std::this_thread::sleep_for(std::chrono::seconds(1)); // sleep 1 second
-        // _greenPipeDrawer->drawNumber("I", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
-        // std::this_thread::sleep_for(std::chrono::seconds(1)); // sleep 1 second
+    if ( player == PLAYER_1_SCORE ) {
+        _greenPipeDrawer->drawNumber("I", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // sleep 1 second
+        _greenPipeDrawer->drawNumber("I", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // sleep 1 second
+        _greenPipeDrawer->drawNumber("I", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // sleep 1 second
 
         // blank out
-        //_greenPipeDrawer->drawNumber(" ", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
+        _greenPipeDrawer->drawNumber(" ", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET);
     }
 }
 
