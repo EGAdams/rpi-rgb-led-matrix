9cdac3767e17af13d2d5c4c7fc5f7afe56917af7
egadams <eg1972@gmail.com>
2023-12-07 13:06:05 -0500

    Fix test_02 display issue

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -276,14 +276,14 @@ int main( int argc, char *argv[]) {
     test_count++;
     // end test_01
 
-    // gameObject->getScoreBoard()->clearScreen();
-    // gameObject->getScoreBoard()->drawText( "Test",  
-    // YELLOW, TEST_TEXT__X__POSITION, TEST_TEXT__Y__POSITION );
-    // gameObject->getScoreBoard()->drawText( " 02 ",  
-    // YELLOW, TEST_NUMBER__X__POSITION, TEST_NUMBER__Y__POSITION );
-    // GameTimer::gameDelay( 4000 );
-    // std::cout << "calling test_02()..." << std::endl;
-    // test_02( gameObject, gameState, &loop_count );
+    gameObject->getScoreBoard()->clearScreen();
+    gameObject->getScoreBoard()->drawText( "Test",  
+    YELLOW, TEST_TEXT__X__POSITION, TEST_TEXT__Y__POSITION );
+    gameObject->getScoreBoard()->drawText( " 02 ",  
+    YELLOW, TEST_NUMBER__X__POSITION, TEST_NUMBER__Y__POSITION );
+    GameTimer::gameDelay( 4000 );
+    std::cout << "calling test_02()..." << std::endl;
+    test_02( gameObject, gameState, &loop_count );
     test_count++;
     // end test_02
     
