6f1a8ed598e70c5955d50c7cd539e8a02eaa7844
egadams <eg1972@gmail.com>
2023-12-07 12:27:48 -0500

    Fix blinking score bug in ScoreBoard.cpp

--- tennis-game/ScoreBoard/ScoreBoard.cpp
+++ tennis-game/ScoreBoard/ScoreBoard.cpp
@@ -191,6 +191,17 @@ void ScoreBoard::blink_player_score(int player) {
     if ( !onRaspberryPi()) /* return if not on Pi */ { return; }
     #define BLINK_DELAY 100
     if ( player == PLAYER_1_SCORE ) {
+        for ( int i=0; i < 5; i++ ) {
+            _greenPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 20 );
+            GameTimer::gameDelay( BLINK_DELAY );
+            _blankPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 20 );
+            GameTimer::gameDelay( BLINK_DELAY );
+            _greenPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 20 );
+            GameTimer::gameDelay( BLINK_DELAY );
+            _blankPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 20 );
+            GameTimer::gameDelay( BLINK_DELAY );
+        }
+    } else {
         // do this 5 times
         for ( int i = 0; i < 5; i++ ) {
             _redPeriodDrawer->drawNumber( ".", RED_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET -20 );
@@ -205,18 +216,7 @@ void ScoreBoard::blink_player_score(int player) {
             GameTimer::gameDelay( BLINK_DELAY );
             _blankPeriodDrawer->drawNumber( ".", RED_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 20 );
         }
-        // _greenPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET -20 );
-        // GameTimer::gameDelay( BLINK_DELAY );
-        // _blankPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET -20 );
-        // GameTimer::gameDelay( BLINK_DELAY );
-        // _greenPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 20 );
-        // GameTimer::gameDelay( BLINK_DELAY );
-        // _blankPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET );
-        // GameTimer::gameDelay( BLINK_DELAY );
-        // _greenPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET );
-        // GameTimer::gameDelay( BLINK_DELAY );
-        // _blankPeriodDrawer->drawNumber( ".", GREEN_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET );
-    }
+    }    
 }
 
 void ScoreBoard::clearScreen() {
