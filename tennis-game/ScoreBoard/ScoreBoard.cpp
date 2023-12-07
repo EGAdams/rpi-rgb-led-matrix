a863e34d296b9f8c5597b62c8fe7cddaa580d198
egadams <eg1972@gmail.com>
2023-12-07 12:46:39 -0500

    Update blink delay in ScoreBoard.cpp

--- tennis-game/ScoreBoard/ScoreBoard.cpp
+++ tennis-game/ScoreBoard/ScoreBoard.cpp
@@ -189,12 +189,12 @@ void ScoreBoard::_drawTieBreakerBar() {
 
 void ScoreBoard::blink_player_score(int player) {
     if ( !onRaspberryPi()) /* return if not on Pi */ { return; }
-    #define BLINK_DELAY 100
+    #define BLINK_DELAY 75
     if ( player == PLAYER_1_SCORE ) {
         for ( int i=0; i < 5; i++ ) {
-            _greenPeriodDrawer->drawNumber( ".", RED_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 10 );
+            _greenPeriodDrawer->drawNumber( ".", RED_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 30 );
             GameTimer::gameDelay( BLINK_DELAY );
-            _blankPeriodDrawer->drawNumber( ".", RED_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 10 );
+            _blankPeriodDrawer->drawNumber( ".", RED_BAR_HORIZONTAL_OFFSET, BLUE_BAR_VERTICAL_OFFSET - 30 );
             GameTimer::gameDelay( BLINK_DELAY );
         }
     } else {
