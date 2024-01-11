3aafa976c0391790217787f0e9c314b21fa9aa15
egadams <eg1972@gmail.com>
2024-01-11 11:13:24 -0500

    Add set history for player 1 and player 2

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -274,6 +274,8 @@ void test_05( GameObject* gameObject, GameState* gameState, int* loop_count ) {
     gameObject->getScoreBoard()->clearScreen();
     gameObject->getPlayer1()->setSetHistory( 1, 6 );
     gameObject->getPlayer2()->setSetHistory( 1, 4 );
+    gameObject->getPlayer1()->setSetHistory( 2, 4 );
+    gameObject->getPlayer2()->setSetHistory( 2, 5 );
     gameObject->getPlayer1()->setGames( 4 );
     gameObject->getPlayer2()->setGames( 5 );
     gameState->setCurrentSet( 2 );
