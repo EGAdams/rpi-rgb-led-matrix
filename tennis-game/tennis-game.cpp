89dd939fce689e163d541dfd64cd3332f56160c6
egadams <eg1972@gmail.com>
2024-01-11 11:16:08 -0500

    Fix player points in test_05 function

--- tennis-game/tennis-game.cpp
+++ tennis-game/tennis-game.cpp
@@ -276,11 +276,11 @@ void test_05( GameObject* gameObject, GameState* gameState, int* loop_count ) {
     gameObject->getPlayer2()->setSetHistory( 1, 4 );
     gameObject->getPlayer1()->setSetHistory( 2, 4 );
     gameObject->getPlayer2()->setSetHistory( 2, 5 );
+    gameState->setCurrentSet( 2 );
     gameObject->getPlayer1()->setGames( 4 );
     gameObject->getPlayer2()->setGames( 5 );
-    gameState->setCurrentSet( 2 );
-    gameObject->getPlayer1()->setPoints( 3 );
-    gameObject->getPlayer2()->setPoints( 2 );
+    gameObject->getPlayer1()->setPoints( 2 );
+    gameObject->getPlayer2()->setPoints( 3 );
     gameObject->getScoreBoard()->update();
 
 }
