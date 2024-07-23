655063caabf3921f4d272f04cb12a71a47c5e812
egadams <eg1972@gmail.com>
2024-07-23 13:08:59 -0400

    changed panel order to go from 7 to 0

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -256,7 +256,7 @@ class TwoSixtyFourMapper : public PixelMapper {
                 //initialize the panels
                 Panel firstPanel;
                 firstPanel.name = "firstPanel";
-                firstPanel.order = 3;
+                firstPanel.order = 7;
                 firstPanel.rotate = 0;
                 firstPanel.y_offset = 0;
                 firstPanel.x_offset = 0;
@@ -264,43 +264,43 @@ class TwoSixtyFourMapper : public PixelMapper {
 
                 Panel secondPanel;
                 secondPanel.name = "secondPanel";
-                secondPanel.order = 2;
+                secondPanel.order = 6;
                 secondPanel.rotate = 0;
                 _panels[ 1 ] = secondPanel;
 
                 Panel thirdPanel;
                 thirdPanel.name = "thirdPanel";
-                thirdPanel.order = 1;
+                thirdPanel.order = 5;
                 thirdPanel.rotate = 0;
                 _panels[ 2 ] = thirdPanel;
 
                 Panel fourthPanel;
                 fourthPanel.name = "fourthPanel";
-                fourthPanel.order = 0;
+                fourthPanel.order = 4;
                 fourthPanel.rotate = 0;
                 _panels[ 3 ] = fourthPanel;
 
                 Panel fifthPanel;
                 fifthPanel.name = "fifthPanel";
-                fifthPanel.order = 7;
+                fifthPanel.order = 3;
                 fifthPanel.rotate = 0;
                 _panels[ 4 ] = fifthPanel;
 
                 Panel sixthPanel;
                 sixthPanel.name = "sixthPanel";
-                sixthPanel.order = 6;
+                sixthPanel.order = 2;
                 sixthPanel.rotate = 0;
                 _panels[ 5 ] = sixthPanel;
 
                 Panel seventhPanel;
                 seventhPanel.name = "seventhPanel";
-                seventhPanel.order = 5;
+                seventhPanel.order = 1;
                 seventhPanel.rotate = 0;
                 _panels[ 6 ] = seventhPanel;
 
                 Panel eighthPanel;
                 eighthPanel.name = "eighthPanel";
-                eighthPanel.order = 4;
+                eighthPanel.order = 0;
                 eighthPanel.rotate = 0;
                 _panels[ 7 ] = eighthPanel; 
                 printf( "264 mapper constructed. \n" ); }
