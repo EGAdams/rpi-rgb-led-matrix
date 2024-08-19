c31e4cea3438b85db1c09cd3d254b0650a04ea8a
egadams <eg1972@gmail.com>
2024-08-19 12:10:42 -0400

    added print statements after match win

--- tennis-game/MatchWinSequence/MatchWinSequence.cpp
+++ tennis-game/MatchWinSequence/MatchWinSequence.cpp
@@ -41,4 +41,5 @@ void MatchWinSequence::run( Player* player, GameState* gameState, GameLeds* game
         // gameState->setStarted( 0 );  // trigger game reset, end run().
         gameState->setCurrentAction( NORMAL_GAME_STATE );
     }
+    std::cout << "match win sequence is done." << std::endl;
 }
