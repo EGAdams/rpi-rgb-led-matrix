e09b922ed60a394059b4deaf8fc50735a3a51401
egadams <eg1972@gmail.com>
2024-09-07 11:51:07 -0400

    reinstated allow score store and load.

--- tennis-game/GameObject/GameObject.cpp
+++ tennis-game/GameObject/GameObject.cpp
@@ -74,10 +74,28 @@ void GameObject::loopGame() {
 
 GameState* GameObject::getGameState() { return _gameState; }
 
+void GameObject::sleepModeDelay() {
+    // Block this thread for 3 seconds
+    std::this_thread::sleep_for(std::chrono::seconds( 3 ));
+
+    // After 3 seconds, allow scoring again
+    _allowScore.store( true );
+}
+
 void GameObject::playerScore( int playerNumber ) {  // sets the gamestate player button
-    if ( _gameState->getCurrentAction() == SLEEP_MODE ) {
+    if ( _gameState->getCurrentAction() == SLEEP_MODE ) { // #define AFTER_MATCH_WIN for this
         std::cout << "*** WARNING: player score during sleep mode, returning... ***" << std::endl;
-        return;
+
+        // Check if the timer is already running
+        if ( !_allowScore.load()) {
+            // The timer is still running, ignore the score
+            return;
+        }
+
+        // Disable scoring and start the timer to allow scoring again after 3 seconds
+        _allowScore.store( false );
+        std::thread( &GameObject::sleepModeDelay, this ).detach();
+        return; // Ignore this score attempt
     }
     _gameState->setCurrentAction( "Updating state after player " + std::to_string( playerNumber ) + " scored." );
     _gameState->setPlayerButton( playerNumber );
