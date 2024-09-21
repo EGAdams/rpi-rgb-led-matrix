786d56f47c00b54939e33552355b5c195751f0d9
egadams <eg1972@gmail.com>
2024-09-21 12:29:33 -0400

    adjust left margin

--- tennis-game/RemotePairingScreen/RemotePairingScreen.cpp
+++ tennis-game/RemotePairingScreen/RemotePairingScreen.cpp
@@ -7,17 +7,17 @@ RemotePairingScreen::RemotePairingScreen( ScoreBoard* scoreboard )
 RemotePairingScreen::~RemotePairingScreen(){}
 
 void RemotePairingScreen::draw() {
-    #define LEFT_MARGIN 10
     if (!_green_player_paired) {
         if ( _scoreboard->onRaspberryPi()) {
+            #define LEFT_MARGIN 10
             _scoreboard->clearScreen();
-            _scoreboard->drawText( "Green",     GREEN, LEFT_MARGIN, 20   );
-            _scoreboard->drawText( "Player",    GREEN, LEFT_MARGIN, 40   );
-            _scoreboard->drawText( "Press",     GREEN, LEFT_MARGIN, 60   );
-            _scoreboard->drawText( "Your",      GREEN, LEFT_MARGIN, 80   );
-            _scoreboard->drawText( "Remote",    GREEN, LEFT_MARGIN, 100  );
-            _scoreboard->drawText( "Green",     GREEN, LEFT_MARGIN, 120  );
-            _scoreboard->drawText( "Button",    GREEN, LEFT_MARGIN, 130  );
+            _scoreboard->drawText( "Green",     GREEN, LEFT_MARGIN + 1, 17  );
+            _scoreboard->drawText( "Player",    GREEN, LEFT_MARGIN + 0, 34  );
+            _scoreboard->drawText( "Press",     GREEN, LEFT_MARGIN + 1, 51  );
+            _scoreboard->drawText( "Your",      GREEN, LEFT_MARGIN + 2, 68  );
+            _scoreboard->drawText( "Remote",    GREEN, LEFT_MARGIN + 0, 85  );
+            _scoreboard->drawText( "Green",     GREEN, LEFT_MARGIN + 1, 102 );
+            _scoreboard->drawText( "Button",    GREEN, LEFT_MARGIN + 0, 119 );
         } else {
             std::cout << "     Green Player\n";
             std::cout << "     Player\n";
