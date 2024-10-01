0f09596b3a9ef6c4baa54ed5bb86753210cd2508
egadams <eg1972@gmail.com>
2024-10-01 14:17:17 -0400

    draw directly from Matrix Display

--- tennis-game/MatrixDisplay/MatrixDisplay.cpp
+++ tennis-game/MatrixDisplay/MatrixDisplay.cpp
@@ -5,6 +5,7 @@ MatrixDisplay::MatrixDisplay( rgb_matrix::RGBMatrix* canvas, Drawer* drawer ) : 
     _currentFont = FontManager::getFont( LITTLE_NUMBER_FONT );  // Assuming FontManager provides default font
     _currentColor = ColorManager::getColor( "WHITE" );  // Default color is white
     _bg_color = ColorManager::getColor( "BLACK" );  // Default background color is black
+    _canvas = canvas;
 }
 MatrixDisplay::~MatrixDisplay() { if ( _canvas ) { delete _canvas; _canvas = nullptr; }}
 
@@ -21,9 +22,9 @@ void MatrixDisplay::setColor( const std::string& colorName ) { _currentColor = C
 
 void MatrixDisplay::drawText( const std::string& text, int x, int y ) {
     if ( !_canvas ) { std::cerr << "Canvas is not initialized. Cannot draw text." << std::endl; return; }
-    // rgb_matrix::DrawText( _canvas, _currentFont, x, y + _currentFont.baseline(), _currentColor, &_bg_color, text.c_str(), 0 );
     print( "inside MatrixDisplay::drawText, calling _drawer->drawText..." );
-    _drawer->drawText( text, x, y );
+    // _drawer->drawText( text, x, y );
+    rgb_matrix::DrawText( _canvas, _currentFont, x, y + _currentFont.baseline(), _currentColor, &_bg_color, text.c_str(), 0 );
     print( "inside MatrixDisplay::drawText, done calling _drawer->drawText..." );
 }
 
