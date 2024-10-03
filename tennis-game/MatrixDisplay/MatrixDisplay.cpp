52750a041798afa0c16706505d816c27a22cc35f
egadams <eg1972@gmail.com>
2024-10-03 10:18:54 -0400

    added debug print

--- tennis-game/MatrixDisplay/MatrixDisplay.cpp
+++ tennis-game/MatrixDisplay/MatrixDisplay.cpp
@@ -22,6 +22,7 @@ void MatrixDisplay::setColor( const std::string& colorName ) { _currentColor = C
 void MatrixDisplay::drawText( const std::string& text, int x, int y ) {
     if ( !_canvas ) { std::cerr << "Canvas is not initialized. Cannot draw text." << std::endl; return; }
     // rgb_matrix::DrawText( _canvas, _currentFont, x, y + _currentFont.baseline(), _currentColor, &_bg_color, text.c_str(), 0 );
+    print( "inside MatrixDisplay::drawText.  about to call _drawer->drawText..." );
     _drawer->drawText( text, x, y );
 }
 
