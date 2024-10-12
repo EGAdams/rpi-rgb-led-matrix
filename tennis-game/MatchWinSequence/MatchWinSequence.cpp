2f41e2b516cea7e42549abdf2d2c6499953a8399
egadams <eg1972@gmail.com>
2024-10-12 14:23:37 -0400

    draw new text!

--- tennis-game/MatchWinSequence/MatchWinSequence.cpp
+++ tennis-game/MatchWinSequence/MatchWinSequence.cpp
@@ -18,8 +18,8 @@ void MatchWinSequence::run( Player* player, GameState* gameState, GameLeds* game
     if ( gameLeds->getScoreBoard()->hasCanvas()) {
         gameLeds->getScoreBoard()->clearScreen();
         gameLeds->getScoreBoard()->setLittleDrawerFont( "fonts/8x13B.bdf" );
-        gameLeds->getScoreBoard()->drawText( "Match", 10, 60  );
-        gameLeds->getScoreBoard()->drawText( "Win"  , 18, 80  );
+        gameLeds->getScoreBoard()->drawNewText( "Match", 10, 60  );
+        gameLeds->getScoreBoard()->drawNewText( "Win"  , 18, 80  );
         gameLeds->getScoreBoard()->drawSets();
         GameTimer::gameDelay( SHOW_MATCH_WIN_TEXT_DELAY );
     } else {
