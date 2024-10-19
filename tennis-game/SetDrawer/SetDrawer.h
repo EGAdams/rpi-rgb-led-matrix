4a2ef4c2364d6b262b17fa5012af999a5ed608c2
egadams <eg1972@gmail.com>
2024-10-19 16:59:12 -0400

    set spacing to zero and fixed set width to 17

--- tennis-game/SetDrawer/SetDrawer.h
+++ tennis-game/SetDrawer/SetDrawer.h
@@ -30,8 +30,8 @@ class SetDrawer {
     void blankSets();
 
  private: 
-    const int FIXED_SET_WIDTH = 20;  // Adjust as needed based on your font size
-    const int SET_SPACING = 5;      // Space between sets
+    const int FIXED_SET_WIDTH = 17;  // Adjust as needed based on your font size
+    const int SET_SPACING = 0;      // Space between sets
     const int OFFSET_FOR_ONE = 2;    // Additional offset for the digit "1"
 
     rgb_matrix::Font    _little_font;
