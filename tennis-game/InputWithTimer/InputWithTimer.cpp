cbd1601500bc1aec2c469129886a00c6cbeb6b21
egadams <eg1972@gmail.com>
2024-12-09 17:52:12 -0500

    changed to 7 and 11 input timer

--- tennis-game/InputWithTimer/InputWithTimer.cpp
+++ tennis-game/InputWithTimer/InputWithTimer.cpp
@@ -21,7 +21,7 @@ int InputWithTimer::getInput() {
         while ( !done ) {                           // remote mode
             selection = _inputs->read_mcp23017_value();
             std::cout << "read selection from inputs: " << selection << std::endl;
-            if ( selection == 8 || selection == 4 ) {
+            if ( selection == 7 || selection == 11 ) {
                 std::cout << "selection: " << selection << " triggered the done flag, exiting while loop..." << std::endl;
                 done = true;
             } else { 
