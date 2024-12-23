46a3c1aa5e32acf2f3b89192073d8ea189b0c581
egadams <eg1972@gmail.com>
2024-12-23 14:55:21 -0500

    took out if need jump

--- tennis-game/Inputs/Inputs.cpp
+++ tennis-game/Inputs/Inputs.cpp
@@ -155,15 +155,15 @@ int Inputs::readPlayerButtons() {
             }
             // below code may be needed for the power cycle
             if ( _need_jump_start ) {
-                print ( "need_jump_start is true.  checking if game is started... " );
-                if ( _gameState->getStarted() ) {
-                    print( "game is started.  resetting need_jump_start to false.  no jump start needed." );
-                    _need_jump_start = false;
-                } else {
-                    print( "game is not started.  we may need a jump start here." );
-                    _need_jump_start = false;
-                    // originalRemoteCode = UNKNOWN_REMOTE_BUTTON; 
-                } 
+                // print ( "need_jump_start is true.  checking if game is started... " );
+                // if ( _gameState->getStarted() ) {
+                //     print( "game is started.  resetting need_jump_start to false.  no jump start needed." );
+                //     _need_jump_start = false;
+                // } else {
+                //     print( "game is not started.  we may need a jump start here." );
+                //     _need_jump_start = false;
+                //     originalRemoteCode = UNKNOWN_REMOTE_BUTTON; 
+                // } 
             }
         }
         print("exited while.  *** CODE IS VALID ***.  returning originalRemoteCode [" << std::to_string( originalRemoteCode ) << "]" );
