f98d06dc3291a531d99feb5c6e1d278481c95591
egadams <eg1972@gmail.com>
2024-12-27 11:45:46 -0500

    switch off remote input

--- tennis-game/TennisConstants/TennisConstants.h
+++ tennis-game/TennisConstants/TennisConstants.h
@@ -26,7 +26,7 @@
 
 
 /////////////////////// REMOTE DEFINITIONS /////////////////////////////////////
-# define REMOTE_INPUT 1 // 122224
+# define REMOTE_INPUT 0 // 122224
 
 #define REMOTE_DATA_0 25
 #define REMOTE_DATA_1 24
