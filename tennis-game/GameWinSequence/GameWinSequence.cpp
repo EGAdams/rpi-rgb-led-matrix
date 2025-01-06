ff07ec1b32a833e83b27d3d41fa4ad2993d566cb
egadams <eg1972@gmail.com>
2025-01-06 14:18:07 -0500

    examine the game flash delays in GameWinSequence::run()

--- tennis-game/GameWinSequence/GameWinSequence.cpp
+++ tennis-game/GameWinSequence/GameWinSequence.cpp
@@ -8,25 +8,28 @@ void GameWinSequence::run( Player* player, GameState* gameState,
     if ( scoreBoard->onRaspberryPi() == false ) { return; }
     print( "not returning out of GameWinSequence::run() because on Raspberry Pi" );
     if ( scoreBoard->hasCanvas()) {
-        print ( "*** scoreboard has canvas, NOT entering blink sequence loop... ***" );
-        return; /* TODO: REMOVE THIS HARD CODE! */
         print ( "scoreboard has canvas, entering blink sequence loop..." );
         for ( int blink_sequence_count = 0; blink_sequence_count < LOOP_GAME_LAMP_WIN; blink_sequence_count++ ) {
             player->number() == PLAYER_1_INITIALIZED ? gameState->setCurrentAction( "player1 blink" ) 
                                 : gameState->setCurrentAction( "player2 blink" );
+            print( "updating scoreboard..." );
             scoreBoard->update();
+            print( "sleeping " << GAME_FLASH_DELAY << " milliseconds..." );
             GameTimer::gameDelay( GAME_FLASH_DELAY );  /*** wait ***/
             gameState->setCurrentAction( NORMAL_GAME_STATE );
             scoreBoard->update(); 
+            print( "sleeping " << GAME_FLASH_DELAY << " milliseconds..." );
             GameTimer::gameDelay( GAME_FLASH_DELAY );  /*** wait ***/ }
     } else {
         print ( "scoreboard does not have canvas, how did we ever get here?" );
         for ( int blink_sequence_count = 0; blink_sequence_count < LOOP_GAME_LAMP_WIN; blink_sequence_count++ ) {  
             player->setGames( 5 );                    // set game count so game LEDs will turn off
             gameLeds->updateGames();                                // actually turn game LEDs off
+            print( "sleeping " << GAME_FLASH_DELAY << " milliseconds..." );
             GameTimer::gameDelay( GAME_FLASH_DELAY );  /*** wait ***/
             player->setGames( games_in_memory );                    // restore game count in player object  
-            gameLeds->updateGames();                                // turn players game point on
+            gameLeds->updateGames(); 
+            print( "sleeping " << GAME_FLASH_DELAY << " milliseconds..." );                              // turn players game point on
             GameTimer::gameDelay( GAME_FLASH_DELAY );  /*** wait ****/ }
     }
     print ( "exiting GameWinSequence::run()" );
