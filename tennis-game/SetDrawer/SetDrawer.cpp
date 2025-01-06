95bc5aac1088c448e7092142ccf671e2bdbcffdd
egadams <eg1972@gmail.com>
2025-01-06 14:20:33 -0500

    remove delay in drawBlinkSets

--- tennis-game/SetDrawer/SetDrawer.cpp
+++ tennis-game/SetDrawer/SetDrawer.cpp
@@ -70,8 +70,8 @@ void SetDrawer::drawBlinkSets( int playerToBlink ) {
         std::cout << playerTwoSetString << std::endl;
     } else {
         drawSetsWithSpacing(playerOneSetString, playerTwoSetString);
-        
-        GameTimer::gameDelay( SET_WIN_FLASH_DELAY );
+        // print( "NOT sleeping " << SET_WIN_FLASH_DELAY << " milliseconds..." );
+        // GameTimer::gameDelay( SET_WIN_FLASH_DELAY );
     }
 }
 
