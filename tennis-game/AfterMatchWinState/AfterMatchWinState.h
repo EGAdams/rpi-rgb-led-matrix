45a1bd8446c522e82b5ac84fd22fdae29c91f1b8
egadams <eg1972@gmail.com>
2025-02-10 11:00:14 -0500

    added after win state folder, .h, and .cpp files

---
+++ tennis-game/AfterMatchWinState/AfterMatchWinState.h
@@ -0,0 +1,21 @@
+#ifndef AFTER_MATCH_WIN_STATE_H
+#define AFTER_MATCH_WIN_STATE_H
+
+#include "../TennisConstants/TennisConstants.h"
+#include "../IRemoteListenerState/IRemoteListenerState.h"
+#include "../RemoteListenerContext/RemoteListenerContext.h"
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+class RegularGamePlayBeforeScoreState : public IRemoteListenerState {
+public:
+    void handleInput(RemoteListenerContext &context) override;
+
+private:
+    void handleSelectionAndUpdate(RemoteListenerContext &context, int selection);
+    void showHelp();
+};
+
+#endif // AFTER_MATCH_WIN_STATE_H
