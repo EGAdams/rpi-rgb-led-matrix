ab0e227ce47ca0ed34896cebf7788f6f907f7d12
egadams <eg1972@gmail.com>
2025-02-13 15:35:12 -0500

    change main input timeout to 20sec.

--- tennis-game/run_remote_listener.cpp
+++ tennis-game/run_remote_listener.cpp
@@ -96,7 +96,7 @@ bool is_on_raspberry_pi() {
  * each iteration.
  ***************************************************************/
 void run_remote_listener( GameObject* gameObject, GameState* gameStatearg, Reset* reset ) {
-    const int MAIN_INPUT_TIMEOUT = 10000; // <---------- set universal timeout here ---------<<
+    const int MAIN_INPUT_TIMEOUT = 20000; // <---------- set universal timeout here ---------<<
     GameState* gameState = gameStatearg;
     RemoteLocker* remoteLocker = new RemoteLocker( gameState );
     bool no_score = true;
